659e95a9e0b585721b59812acf7527408ecd0ae5
Bricktop <marcel.mueller1@rwth-aachen.de>
2018-07-05 09:58:16 +0200

    Adds release controller and resourceset (#3)
    * Add Helm chart
    
    * address review comments
    
    * Adds release controller and resourceset
    
    * add TODO
    
    * fix fmt
    
    * address review comments

---
+++ service/controller/release.go
@@ -0,0 +1,96 @@
+package controller
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/controller"
+	"github.com/giantswarm/operatorkit/informer"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/release-operator/service/controller/v1"
+)
+
+type ReleaseConfig struct {
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
+
+	ProjectName string
+}
+
+type Release struct {
+	*controller.Controller
+}
+
+func NewRelease(config ReleaseConfig) (*Release, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+
+	var err error
+
+	var newInformer *informer.Informer
+	{
+		c := informer.Config{
+			Logger: config.Logger,
+
+			Watcher: config.K8sClient.CoreV1().ConfigMaps("release-operator"),
+		}
+
+		newInformer, err = informer.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var v1ResourceSet *controller.ResourceSet
+	{
+		c := v1.ResourceSetConfig{
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		v1ResourceSet, err = v1.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceRouter *controller.ResourceRouter
+	{
+		c := controller.ResourceRouterConfig{
+			Logger: config.Logger,
+			ResourceSets: []*controller.ResourceSet{
+				v1ResourceSet,
+			},
+		}
+
+		resourceRouter, err = controller.NewResourceRouter(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var releaseController *controller.Controller
+	{
+		c := controller.Config{
+			Informer:       newInformer,
+			Logger:         config.Logger,
+			ResourceRouter: resourceRouter,
+			RESTClient:     config.K8sClient.CoreV1().RESTClient(),
+
+			Name: config.ProjectName,
+		}
+
+		releaseController, err = controller.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	c := &Release{
+		Controller: releaseController,
+	}
+
+	return c, nil
+}
---
+++ service/controller/v1/resource_set.go
@@ -0,0 +1,91 @@
+package v1
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/controller"
+	"github.com/giantswarm/operatorkit/controller/resource/metricsresource"
+	"github.com/giantswarm/operatorkit/controller/resource/retryresource"
+	"k8s.io/client-go/kubernetes"
+)
+
+type ResourceSetConfig struct {
+	// Dependencies.
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
+
+	// Settings.
+	ProjectName string
+}
+
+func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
+	var err error
+
+	// TODO: implement configmap,secret,chartConfig resource
+	resources := []controller.Resource{}
+
+	{
+		c := retryresource.WrapConfig{
+			Logger: config.Logger,
+		}
+
+		resources, err = retryresource.Wrap(resources, c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	{
+		c := metricsresource.WrapConfig{
+			Name: config.ProjectName,
+		}
+
+		resources, err = metricsresource.Wrap(resources, c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	initCtxFunc := func(ctx context.Context, obj interface{}) (context.Context, error) {
+		return ctx, nil
+	}
+
+	handlesFunc := func(obj interface{}) bool {
+		// TODO: implement key package
+
+		return true
+	}
+
+	var resourceSet *controller.ResourceSet
+	{
+		c := controller.ResourceSetConfig{
+			Handles:   handlesFunc,
+			InitCtx:   initCtxFunc,
+			Logger:    config.Logger,
+			Resources: resources,
+		}
+
+		resourceSet, err = controller.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	return resourceSet, nil
+}
+
+func toCRUDResource(logger micrologger.Logger, ops controller.CRUDResourceOps) (*controller.CRUDResource, error) {
+	c := controller.CRUDResourceConfig{
+		Logger: logger,
+		Ops:    ops,
+	}
+
+	r, err := controller.NewCRUDResource(c)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return r, nil
+}
