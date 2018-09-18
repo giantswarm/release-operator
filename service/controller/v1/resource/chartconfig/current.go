597ca67041fb2d28d8337449cf8e339afe07743b
Tim Schindler <xh3b4sd@gmail.com>
2018-09-18 17:01:59 +0200

    adds controller context usage (#19)

--- service/controller/v1/resource/chartconfig/current.go
+++ service/controller/v1/resource/chartconfig/current.go
@@ -1,7 +1,30 @@
 package chartconfig
 
-import "context"
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
+
+	"github.com/giantswarm/release-operator/service/controller/v1/controllercontext"
+)
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	err = cc.Validate()
+	if controllercontext.IsInvalidContext(err) {
+		// In case the controller context is not valid we miss certain information
+		// necessary to compute the chart config CRs. In this case we stop here.
+		r.logger.LogCtx(ctx, "level", "debug", "message", "invalid controller context")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "cannot compute chart config CR")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+		resourcecanceledcontext.SetCanceled(ctx)
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
 	return nil, nil
 }
