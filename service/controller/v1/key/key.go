82a8406a43e47e06fa5e37d1ce100ee9969799a7
Tim Schindler <xh3b4sd@gmail.com>
2018-09-18 13:43:41 +0200

    adds initial chart CR structure

--- service/controller/v1/key/key.go
+++ service/controller/v1/key/key.go
@@ -1,9 +1,41 @@
 package key
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
 	"github.com/giantswarm/microerror"
 )
+
+const (
+	LabelApp          = "app"
+	LabelManagedBy    = "giantswarm.io/managed-by"
+	LabelOrganization = "giantswarm.io/organization"
+	LabelServiceType  = "giantswarm.io/service-type"
+)
+
+const (
+	ProjectName        = "release-operator"
+	OrganizationName   = "giantswarm"
+	ServiceTypeManaged = "managed"
+)
+
+func OperatorChannelName(customResource v1alpha1.Release) string {
+	return strings.Replace(OperatorVersion(customResource), ".", "-", -1)
+}
+
+func OperatorChartName(customResource v1alpha1.Release) string {
+	return fmt.Sprintf("%s-chart", OperatorName(customResource))
+}
+
+func OperatorName(customResource v1alpha1.Release) string {
+	return customResource.Spec.Operator.Name
+}
+
+func OperatorVersion(customResource v1alpha1.Release) string {
+	return customResource.Spec.Operator.Version
+}
 
 func ToCustomResource(v interface{}) (v1alpha1.Release, error) {
 	customResourcePointer, ok := v.(*v1alpha1.Release)
