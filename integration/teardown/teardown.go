2746d5cfa4e74e71abc76030eea125d6a23b551b
MarcelMue <marcel.mueller1@rwth-aachen.de>
2018-09-20 12:17:15 +0200

    improve error masking

--- integration/teardown/teardown.go
+++ integration/teardown/teardown.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/giantswarm/e2e-harness/pkg/framework"
 	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/microerror"
 )
 
 func Teardown(h *framework.Host, helmClient *helmclient.Client) error {
 	err := framework.HelmCmd(fmt.Sprintf("delete %s-release-operator --purge", h.TargetNamespace()))
 	if err != nil {
-		return err
+		return microerror.Mask(err)
 	}
 
 	return nil
