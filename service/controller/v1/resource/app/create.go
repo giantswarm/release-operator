7149c54bea54b8c96e867f1ff594af0c589d65a8
Theo Brigitte <theo.brigitte@gmail.com>
2019-01-17 20:03:38 +0100

    add app resource

---
+++ service/controller/v1/resource/app/create.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"context"
+
+	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
+	appCRs, err := toAppCRs(createChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if len(appCRs) != 0 {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "creating App CRs in the Kubernetes API")
+
+		for _, c := range appCRs {
+			_, err = r.g8sClient.ApplicationV1alpha1().Apps(r.namespace).Create(c)
+			if apierrors.IsAlreadyExists(err) {
+				// fall through
+			} else if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "created App CRs in the Kubernetes API")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "App CRs do not have to be created in the Kubernetes API")
+	}
+
+	return nil
+}
+
+func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentAppCRs, err := toAppCRs(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredAppCRs, err := toAppCRs(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var appCRsToCreate []*applicationv1alpha1.App
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "computing create state")
+
+		for _, d := range desiredAppCRs {
+			if !containsAppCRs(currentAppCRs, d) {
+				appCRsToCreate = append(appCRsToCreate, d)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "computed create state")
+	}
+
+	return appCRsToCreate, nil
+}
---
+++ service/controller/v1/resource/app/update.go
@@ -0,0 +1,99 @@
+package app
+
+import (
+	"context"
+	"reflect"
+
+	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller"
+)
+
+func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
+	appCRsToUpdate, err := toAppCRs(updateChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if len(appCRsToUpdate) != 0 {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "updating App CRs in the Kubernetes API")
+
+		for _, c := range appCRsToUpdate {
+			_, err = r.g8sClient.ApplicationV1alpha1().Apps(r.namespace).Update(c)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "updated App CRs in the Kubernetes API")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "App CRs do not have to be updated in the Kubernetes API")
+	}
+
+	return nil
+}
+
+func (r *Resource) NewUpdatePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*controller.Patch, error) {
+	create, err := r.newCreateChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	update, err := r.newUpdateChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := controller.NewPatch()
+	patch.SetCreateChange(create)
+	patch.SetUpdateChange(update)
+
+	return patch, nil
+}
+
+func (r *Resource) newUpdateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentAppCRs, err := toAppCRs(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredAppCRs, err := toAppCRs(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var appCRsToUpdate []*applicationv1alpha1.App
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "computing update state")
+
+		for _, c := range currentAppCRs {
+			d, err := getAppCRByName(desiredAppCRs, c.Name)
+			if IsNotFound(err) {
+				continue
+			} else if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			if isAppCRModified(d, c) {
+				u := d.DeepCopy()
+				u.ObjectMeta.ResourceVersion = c.ObjectMeta.ResourceVersion
+
+				appCRsToUpdate = append(appCRsToUpdate, u)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "computed update state")
+	}
+
+	return appCRsToUpdate, nil
+}
+
+func isAppCRModified(a, b *applicationv1alpha1.App) bool {
+	if !reflect.DeepEqual(a.Spec, b.Spec) {
+		return true
+	}
+	if !reflect.DeepEqual(a.Labels, b.Labels) {
+		return true
+	}
+
+	return false
+}
