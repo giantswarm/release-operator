cd137c122c6edae551c8abe2e6faf032da05d236
Theo Brigitte <theo.brigitte@gmail.com>
2019-02-14 18:06:47 +0100

    update app resource

--- service/controller/releasecycle/resource/app/resource.go
+++ service/controller/releasecycle/resource/app/resource.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
@@ -15,7 +14,6 @@ const (
 
 // Config represents the configuration used to create a new app resource.
 type Config struct {
-	// Dependencies.
 	G8sClient versioned.Interface
 	K8sClient kubernetes.Interface
 	Logger    micrologger.Logger
@@ -24,8 +22,13 @@ type Config struct {
 }
 
 // Resource implements the app resource.
+//
+// It ensures each release cycle has its corresponding release installed.
+// It does so by creating an App CR for the release, which will then be
+// installed by app-operator.
+// Note: release are never removed, so removing a release cycle CR has no effect
+// 	 on the previously installed release.
 type Resource struct {
-	// Dependencies.
 	g8sClient versioned.Interface
 	k8sClient kubernetes.Interface
 	logger    micrologger.Logger
@@ -35,7 +38,6 @@ type Resource struct {
 
 // New creates a new configured app resource.
 func New(config Config) (*Resource, error) {
-	// Dependencies.
 	if config.G8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
 	}
@@ -51,7 +53,6 @@ func New(config Config) (*Resource, error) {
 	}
 
 	r := &Resource{
-		// Dependencies.
 		g8sClient: config.G8sClient,
 		k8sClient: config.K8sClient,
 		logger:    config.Logger,
@@ -64,26 +65,4 @@ func New(config Config) (*Resource, error) {
 
 func (r *Resource) Name() string {
 	return Name
-}
-
-func getAppCR(list []*applicationv1alpha1.App, namespace, name string) (*applicationv1alpha1.App, bool) {
-	for _, l := range list {
-		b := true
-		b = b && l.Namespace == namespace
-		b = b && l.Name == name
-		if b {
-			return l, true
-		}
-	}
-
-	return nil, false
-}
-
-func toAppCRs(v interface{}) ([]*applicationv1alpha1.App, error) {
-	appCRs, ok := v.([]*applicationv1alpha1.App)
-	if !ok {
-		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", []*applicationv1alpha1.App{}, v)
-	}
-
-	return appCRs, nil
 }
