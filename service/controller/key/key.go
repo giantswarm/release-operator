3f662fcad48806407092b568b1700bbf3d9edd52
Theo Brigitte <theo.brigitte@gmail.com>
2019-02-14 17:53:58 +0100

    add key.ToAppCR

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -1,6 +1,7 @@
 package key
 
 import (
+	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/microerror"
 )
@@ -24,12 +25,20 @@ func ReleaseVersion(customResource releasev1alpha1.Release) string {
 	return customResource.Spec.Version
 }
 
+func ToAppCR(v interface{}) (*applicationv1alpha1.App, error) {
+	appCR, ok := v.(*applicationv1alpha1.App)
+	if !ok {
+		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", applicationv1alpha1.App{}, v)
+	}
+
+	return appCR, nil
+}
+
 func ToReleaseCycleCR(v interface{}) (releasev1alpha1.ReleaseCycle, error) {
-	customResourcePointer, ok := v.(*releasev1alpha1.ReleaseCycle)
+	releaseCycleCR, ok := v.(*releasev1alpha1.ReleaseCycle)
 	if !ok {
 		return releasev1alpha1.ReleaseCycle{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &releasev1alpha1.ReleaseCycle{}, v)
 	}
-	customResource := *customResourcePointer
 
-	return customResource, nil
+	return *releaseCycleCR, nil
 }
