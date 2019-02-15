7cbee9c8aa6c5d2132c6df95bc6d57722ef5605e
Paweł Kopiczko <kopiczko@gmail.com>
2019-02-15 15:48:32 +0000

    controller: use default Handles function

--- service/controller/release/resource_set.go
+++ service/controller/release/resource_set.go
@@ -92,15 +92,10 @@ func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
 		return ctx, nil
 	}
 
-	handlesFunc := func(obj interface{}) bool {
-		return true
-	}
-
 	var resourceSet *controller.ResourceSet
 	{
 		c := controller.ResourceSetConfig{
 			InitCtx:   initCtxFunc,
-			Handles:   handlesFunc,
 			Logger:    config.Logger,
 			Resources: resources,
 		}
