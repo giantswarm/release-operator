a70c359fb803a08a39c4d2fc379158cc89baa3ea
Théo Brigitte <theo.brigitte@gmail.com>
2019-02-20 22:49:35 +0100

    add key functions to manipulate release cycle (#57)
    * add key functions to manipulate release cycle
    
    * add key.invalidReleaseNameError
    
    * fix releasePrefix > ReleasePrefix
    
    * move ReleaseAppCRName and ReleasePrefix to app resource
    
    * check for empty provider or version
    
    * add SplitReleaseName test

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -1,6 +1,8 @@
 package key
 
 import (
+	"strings"
+
 	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/microerror"
@@ -21,10 +23,26 @@ const (
 	ServiceTypeManaged = "managed"
 )
 
-func ReleaseVersion(customResource releasev1alpha1.Release) string {
-	return customResource.Spec.Version
+// ReleaseVersion returns the version of the given release.
+func ReleaseVersion(releaseCR releasev1alpha1.Release) string {
+	return releaseCR.Spec.Version
 }
 
+// SplitReleaseName splits a release name into provider and version.
+// It returns provider, version, and error, in this order.
+//
+// It expects name to be in the following format <provider>.<version>
+// e.g. aws.v6.0.1
+func SplitReleaseName(name string) (string, string, error) {
+	split := strings.SplitN(name, ".", 2)
+	if len(split) < 2 || len(split[0]) == 0 || len(split[1]) == 0 {
+		return "", "", microerror.Maskf(invalidReleaseNameError, "expect <provider>.<version>, got %#q", name)
+	}
+
+	return split[0], split[1], nil
+}
+
+// ToAppCR converts v into an App CR.
 func ToAppCR(v interface{}) (*applicationv1alpha1.App, error) {
 	appCR, ok := v.(*applicationv1alpha1.App)
 	if !ok {
@@ -34,6 +52,7 @@ func ToAppCR(v interface{}) (*applicationv1alpha1.App, error) {
 	return appCR, nil
 }
 
+// ToReleaseCycleCR converts v into a ReleaseCycle CR.
 func ToReleaseCycleCR(v interface{}) (releasev1alpha1.ReleaseCycle, error) {
 	releaseCycleCR, ok := v.(*releasev1alpha1.ReleaseCycle)
 	if !ok {
