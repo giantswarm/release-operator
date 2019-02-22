8cd245be2585f77c742fc2de0c1faa7cc826d18b
Theo Brigitte <theo.brigitte@gmail.com>
2019-02-22 13:09:39 +0100

    configure app kubeconfig to install release app CR in the control plane

--- service/controller/releasecycle/resource/app/desired.go
+++ service/controller/releasecycle/resource/app/desired.go
@@ -48,6 +48,9 @@ func (r *Resource) newAppCR(name, repository, version string) *applicationv1alph
 		},
 		Spec: applicationv1alpha1.AppSpec{
 			Catalog:   r.appCatalog,
+			KubeConfig: applicationv1alpha1.AppSpecKubeConfig{
+				InCluster: true,
+			},
 			Name:      repository,
 			Namespace: r.namespace,
 			Version:   version,
