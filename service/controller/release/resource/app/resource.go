9cf74bbdaf10bb614dae5b7876f05b36087f8882
Paweł Kopiczko <kopiczko@gmail.com>
2019-03-11 15:27:04 +0000

    add godoc

--- service/controller/release/resource/app/resource.go
+++ service/controller/release/resource/app/resource.go
@@ -21,6 +21,10 @@ type Config struct {
 	AppCatalog string
 }
 
+// New returns a resource creating/updating App CRs for components in non-EOL
+// releases and deleting App CRs for components in EOL releases. App CRs for
+// components existing in both EOL and non-EOL releases are not deleted by the
+// returned resource.
 func New(config Config) (controller.Resource, error) {
 	if config.G8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
