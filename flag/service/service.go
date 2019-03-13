0723a69fc4a56108425f150c1678dff1adf9dd75
Ross Fairbanks <ross@giantswarm.io>
2019-03-13 15:49:02 +0100

    Flags changes

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,6 +1,6 @@
 package service
 
-import "github.com/giantswarm/release-operator/flag/service/kubernetes"
+import "github.com/giantswarm/operatorkit/flag/service/kubernetes"
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
