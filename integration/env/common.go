3175477385b1a64aa499c2ae07210f3d29b734b0
Paweł Kopiczko <kopiczko@gmail.com>
2019-04-11 07:59:26 +0100

    integration/env, integration/setup: update env vars (#84)
    Towards https://github.com/giantswarm/giantswarm/issues/5278.

--- integration/env/common.go
+++ integration/env/common.go
@@ -3,37 +3,44 @@ package env
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
-	// EnvVarCircleCI is the process environment variable representing the
-	// CIRCLECI env var.
-	EnvVarCircleCI = "CIRCLECI"
-	// EnvVarCircleSHA is the process environment variable representing the
-	// CIRCLE_SHA1 env var.
-	EnvVarCircleSHA = "CIRCLE_SHA1"
-	// EnvVarKeepResources is the process environment variable representing the
-	// KEEP_RESOURCES env var.
-	EnvVarKeepResources = "KEEP_RESOURCES"
-	// EnvVarRegistryPullSecret is the process environment variable representing the
-	// REGISTRY_PULL_SECRET env var.
+	EnvVarCircleCI           = "CIRCLECI"
+	EnvVarCircleSHA          = "CIRCLE_SHA1"
+	EnvVarE2EKeepResources   = "E2E_KEEP_RESOURCES"
+	EnvVarE2ETestDir         = "E2E_TEST_DIR"
+	EnvVarGithubBotToken     = "GITHUB_BOT_TOKEN"
 	EnvVarRegistryPullSecret = "REGISTRY_PULL_SECRET"
 )
 
 var (
 	circleCI           string
 	circleSHA          string
+	githubToken        string
 	keepResources      string
 	registryPullSecret string
+	testDir            string
 )
 
 func init() {
 	circleCI = os.Getenv(EnvVarCircleCI)
-	keepResources = os.Getenv(EnvVarKeepResources)
+	keepResources = os.Getenv(EnvVarE2EKeepResources)
 
 	circleSHA = os.Getenv(EnvVarCircleSHA)
 	if circleSHA == "" {
 		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarCircleSHA))
+	}
+
+	testDir = os.Getenv(EnvVarE2ETestDir)
+	if testDir == "" {
+		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarE2ETestDir))
+	}
+
+	githubToken = os.Getenv(EnvVarGithubBotToken)
+	if githubToken == "" {
+		panic(fmt.Sprintf("env var %q must not be empty", EnvVarGithubBotToken))
 	}
 
 	registryPullSecret = os.Getenv(EnvVarRegistryPullSecret)
@@ -42,18 +49,26 @@ func init() {
 	}
 }
 
-func CircleCI() string {
-	return circleCI
+func CircleCI() bool {
+	return strings.ToLower(circleCI) == "true"
 }
 
 func CircleSHA() string {
 	return circleSHA
 }
 
-func KeepResources() string {
-	return keepResources
+func KeepResources() bool {
+	return keepResources == strings.ToLower("true")
+}
+
+func GithubToken() string {
+	return githubToken
 }
 
 func RegistryPullSecret() string {
 	return registryPullSecret
+}
+
+func TestDir() string {
+	return testDir
 }
