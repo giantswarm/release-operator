9c472210c07e97d4a145c9f939eb11d0b7212736
Paweł Kopiczko <kopiczko@gmail.com>
2019-04-12 10:38:54 +0100

    integration: bring up tests setup to the latest advancements
    Towards https://github.com/giantswarm/giantswarm/issues/5278.

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -1,74 +1,96 @@
+// +build k8srequired
+
 package setup
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 
-	"github.com/giantswarm/e2e-harness/pkg/framework"
+	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
+	"github.com/giantswarm/e2e-harness/pkg/release"
 	"github.com/giantswarm/e2etemplates/pkg/chartvalues"
-	"github.com/giantswarm/helmclient"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/micrologger"
 
 	"github.com/giantswarm/release-operator/integration/env"
-	"github.com/giantswarm/release-operator/integration/teardown"
+	"github.com/giantswarm/release-operator/service/controller/key"
 )
 
-func WrapTestMain(h *framework.Host, helmClient *helmclient.Client, l micrologger.Logger, m *testing.M) (int, error) {
-	var v int
-	var err error
-	var errors []error
+func Setup(m *testing.M, config Config) {
+	ctx := context.Background()
 
-	err = h.CreateNamespace("giantswarm")
+	v, err := setup(ctx, m, config)
 	if err != nil {
-		errors = append(errors, err)
-		v = 1
+		config.logger.LogCtx(ctx, "level", "error", "message", "failed to setup test environment", "stack", fmt.Sprintf("%#v", err))
+		os.Exit(1)
 	}
 
-	err = helmClient.EnsureTillerInstalled(context.Background())
-	if err != nil {
-		errors = append(errors, err)
-		v = 1
-	}
+	os.Exit(v)
+}
 
-	var values string
+func setup(ctx context.Context, m *testing.M, config Config) (int, error) {
+	ctx := context.Background()
+
+	// Create namespace.
 	{
-		c := chartvalues.ReleaseOperatorConfig{
-			RegistryPullSecret: env.RegistryPullSecret(),
-		}
-		values, err = chartvalues.NewReleaseOperator(c)
+		err := config.K8sSetup.EnsureNamespaceCreated(key.Namespace)
 		if err != nil {
-			errors = append(errors, err)
-			v = 1
+			return microerror.Mask(err)
+		}
+
+		if !env.CircleCI() && !env.KeepResources() {
+			defer func() {
+				err := config.K8sSetup.EnsureNamespaceDeleted(key.Namespace)
+				if err != nil {
+					receiver.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("failed to delete namespace %#q", key.Namespace), "stack", fmt.Sprintf("%#v", err))
+				}
+			}()
 		}
 	}
 
-	err = h.InstallBranchOperator("release-operator", "releasecycle", values)
-	if err != nil {
-		errors = append(errors, err)
-		v = 1
-	}
+	// Install release-operator.
+	{
+		releaseName := "release-operator"
+		chartInfo := release.NewChartInfo("release-operator-chart", env.CircleSHA())
 
-	if v == 0 {
-		v = m.Run()
-	}
-
-	if !env.KeepResources() {
-		// Only do full teardown when not on CI.
-		if !env.CircleCI() {
-			err := teardown.Teardown(h, helmClient)
-			if err != nil {
-				errors = append(errors, err)
-				v = 1
+		var values string
+		{
+			c := chartvalues.ReleaseOperatorConfig{
+				RegistryPullSecret: env.RegistryPullSecret(),
 			}
-			// TODO there should be error handling for the framework teardown.
-			h.Teardown()
+
+			values, err = chartvalues.NewReleaseOperator(c)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		installConditions := []release.ConditionFunc{
+			config.Release.Condition().CRDExists(ctx, releasev1alpha1.NewReleaseCRD()),
+			config.Release.Condition().CRDExists(ctx, releasev1alpha1.NewReleaseCycleCRD()),
+		}
+
+		err = config.Release.EnsureInstalled(ctx, releaseName, chartInfo, values, installConditions...)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if !env.CircleCI() && !env.KeepResources() {
+			defer func() {
+				deleteConditions := []release.ConditionFunc{
+					config.Release.Condition().CRDNotFound(ctx, releasev1alpha1.NewReleaseCRD()),
+					config.Release.Condition().CRDNotFound(ctx, releasev1alpha1.NewReleaseCycleCRD()),
+				}
+
+				err := config.Release.EnsureDeleted(ctx, releaseName, deleteConditions...)
+				if err != nil {
+					receiver.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("failed to delete helm release %#q", releaseName), "stack", fmt.Sprintf("%#v", err))
+				}
+			}()
 		}
 	}
 
-	if len(errors) > 0 {
-		return v, microerror.Mask(errors[0])
-	}
-
-	return v, nil
+	v := m.Run()
+	return v
 }
--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -3,10 +3,38 @@
 package basic
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/release-operator/integration/setup"
 )
+
+var (
+	config setup.Config
+)
+
+func init() {
+	err := initMainTest()
+	if err != nil {
+		return panic(fmt.Sprintf("%#v", err))
+	}
+}
+
+func initMainTest() error {
+	var err error
+
+	{
+		config, err = setup.NewConfig()
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+}
 
 // TestMain allows us to have common setup and teardown steps that are run
 // once for all the tests https://golang.org/pkg/testing/#hdr-Main.
 func TestMain(m *testing.M) {
+	setup.Setup(m, config)
 }
