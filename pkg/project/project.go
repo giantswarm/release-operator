530ae9174f8ae03022ba41f994deccb02d57ac92
Paweł Kopiczko <pawel@giantswarm.io>
2020-04-15 16:15:37 +0100

    Update pkg/project/project.go

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "release-operator"
 	source      = "https://github.com/giantswarm/release-operator"
-	version     = "1.0.0"
+	version     = "1.0.1"
 )
 
 func Description() string {
