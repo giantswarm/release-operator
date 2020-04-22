9b4cfa94260d33be69254e9e8e3ba1e4550fb337
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-22 16:20:21 +0200

    Release v1.0.3

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "release-operator"
 	source      = "https://github.com/giantswarm/release-operator"
-	version     = "1.0.3-dev"
+	version     = "1.0.3"
 )
 
 func Description() string {
