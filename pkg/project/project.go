f6ef30e009b60625b87691d0f88b584b0a0b9c44
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-22 16:26:51 +0200

    Reset version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "release-operator"
 	source      = "https://github.com/giantswarm/release-operator"
-	version     = "1.0.3"
+	version     = "1.0.4-dev"
 )
 
 func Description() string {
