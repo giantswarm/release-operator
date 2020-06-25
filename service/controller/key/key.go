595a46253cb09c4b3bb2b84ad4bc0f93b9e1ae41
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-06-25 14:34:12 +0200

    Go mod tidy & cleanup

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -6,7 +6,6 @@ import (
 
 	applicationv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
-	"github.com/giantswarm/microerror"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/release-operator/pkg/project"
@@ -26,10 +25,6 @@ const (
 
 	ValueServiceTypeManaged = "managed"
 )
-
-type DeletionTimestampGetter interface {
-	GetDeletionTimestamp() *metav1.Time
-}
 
 func BuildAppName(operatorName, operatorRef string) string {
 	return fmt.Sprintf("%s-%s-hackathon", operatorName, operatorRef)
@@ -90,33 +85,4 @@ func ConstructApp(operatorName, operatorRef string) applicationv1alpha1.App {
 			Version:   operatorRef,
 		},
 	}
-}
-
-func IsDeleted(cr DeletionTimestampGetter) bool {
-	return cr.GetDeletionTimestamp() != nil
-}
-
-// ReleaseVersion returns the version of the given release.
-func ReleaseVersion(releaseCR releasev1alpha1.Release) string {
-	return releaseCR.Name
-}
-
-// ToAppCR converts v into an App CR.
-func ToAppCR(v interface{}) (*applicationv1alpha1.App, error) {
-	x, ok := v.(*applicationv1alpha1.App)
-	if !ok {
-		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", x, v)
-	}
-
-	return x, nil
-}
-
-// ToReleaseCR converts v into a Release CR.
-func ToReleaseCR(v interface{}) (*releasev1alpha1.Release, error) {
-	x, ok := v.(*releasev1alpha1.Release)
-	if !ok {
-		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", x, v)
-	}
-
-	return x, nil
 }
