527cf0e1a95dbfdf8abd43faf679856dae596744
Zach Stone <zach@giantswarm.io>
2020-07-01 13:57:46 +0200

    Add helper function to satisfy linting

--- service/collector/helper.go
+++ service/collector/helper.go
@@ -2,6 +2,7 @@ package collector
 
 import (
 	"github.com/giantswarm/k8sclient/v3/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 )
 
@@ -13,4 +14,20 @@ type helperConfig struct {
 type helper struct {
 	clients k8sclient.Interface
 	logger  micrologger.Logger
+}
+
+func newHelper(config helperConfig) (*helper, error) {
+	if config.Clients == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Clients must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	h := &helper{
+		clients: config.Clients,
+		logger:  config.Logger,
+	}
+
+	return h, nil
 }
