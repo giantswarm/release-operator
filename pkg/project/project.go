c9460c90f5b374e93e509cc948e69c9423f1f31d
GitHub Action <action@github.com>
2021-02-16 10:30:49 +0000

    Bump version to 2.2.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "release-operator"
 	source      = "https://github.com/giantswarm/release-operator"
-	version     = "2.2.0"
+	version     = "2.2.1-dev"
 )
 
 func Description() string {
