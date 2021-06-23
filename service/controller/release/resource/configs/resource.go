1493ae9e64f722aff9d52fdc530e8fbee0e413ec
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-06-23 21:15:50 +0200

    Use latest operatorkit

--- service/controller/release/resource/configs/resource.go
+++ service/controller/release/resource/configs/resource.go
@@ -6,7 +6,7 @@ import (
 
 	releasev1alpha1 "github.com/giantswarm/apiextensions/v2/pkg/apis/release/v1alpha1"
 	corev1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/core/v1alpha1"
-	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
+	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
