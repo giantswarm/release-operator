23f6477d3ec2d3861dfbc8881180f1d66f115705
taylorbot <dev@giantswarm.io>
2025-06-24 11:28:42 +0000

    Release v4.2.1

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "release-operator"
 	source      = "https://github.com/giantswarm/release-operator"
-	version     = "4.2.1-dev"
+	version     = "4.2.1"
 )
 
 func Description() string {
